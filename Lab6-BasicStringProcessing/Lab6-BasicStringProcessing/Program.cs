dff4c0b0c44b9a243a973f34654c33fdfce330d0
Justin Willow <justinjwillow@gmail.com>
2023-07-27 18:47:20 -0700

    Implemented split and join functionality

---
+++ Lab6-BasicStringProcessing/Lab6-BasicStringProcessing/Program.cs
@@ -0,0 +1,50 @@
+﻿bool shouldContinue = true;
+
+while (shouldContinue)
+{
+    Console.Write("Enter a sentence: ");
+    string[] userSentence = Console.ReadLine().Split(" ");
+
+    foreach (string word in userSentence)
+    {
+        Console.WriteLine(word);
+    }
+
+    shouldContinue = ContinueChecker();
+}
+
+bool shouldContinue2 = true;
+List<string> listText = new List<string>();
+
+while (shouldContinue2)
+{
+    Console.Write("Enter some text: ");
+    listText.Add(Console.ReadLine());
+
+    string joinedText = string.Join(" ", listText);
+    Console.WriteLine($"You have entered: {joinedText}");
+
+    shouldContinue2 = ContinueChecker();
+}
+static bool ContinueChecker()
+{
+    Console.Write("Would you like to continue? (Y/N): ");
+    string conInput;
+
+    while (true)
+    {
+        conInput = Console.ReadLine().ToLower();
+
+        if (conInput == "y")
+        {
+            return true;
+        }
+        else if (conInput == "n")
+        {
+            Console.WriteLine("Goodbye!");
+            return false;
+        }
+
+        Console.Write("Invalid input. Please answer with either 'Y' or 'N': ");
+    }
+}
